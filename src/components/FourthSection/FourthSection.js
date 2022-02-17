14fa193b7095f2215defeefcdcabab54fb436f93
Kyle <kylevhuang@gmail.com>
2022-02-16 17:55:52 -0800

    d

--- src/components/FourthSection/FourthSection.js
+++ src/components/FourthSection/FourthSection.js
@@ -6,7 +6,7 @@ function FourthSection() {
         <div className="outer background-white second">
           <div className="contentd">
             <h2>Skills</h2>
-            <div className="skill-gri">
+            <div className="skill-grid">
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
                 alt="JS"
